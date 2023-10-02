33f4bcc93f4e93288d3b4555f6eab57c96ba6698
H <rmtyt4@gmail.com>
2023-10-02 16:19:08 +0900

    20231002_CW

---
+++ Cpp_STACK_CLASS/Cpp_STACK_CLASS/Cpp_STACK_CLASS/Cpp_STACK_CLASS.cpp
@@ -0,0 +1,80 @@
+﻿
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Stack {
+private:
+    vector<int> name;
+
+public:
+    
+    void push(int number) {
+        name.push_back(number);
+        cout << "Pushed"<<endl;
+    }
+    int pop() {
+        if (name.size() > 0) {
+            int poppedValue = name.back();
+            name.pop_back();
+            cout << "Popped " << poppedValue << endl;
+            return poppedValue;
+        }
+        else {
+            cout << "Stack is empty. Cannot pop." << endl;
+            return 0; 
+        }
+        }
+    void display() const {
+        for (int num : name) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+};
+
+
+int main()
+{
+    Stack stack;
+    
+    int command;
+    int push_number;
+
+    while (true) {
+
+        cout << "0: Exit" << endl;
+        cout << "1: Push" << endl;
+        cout << "2: Pop" << endl;
+        cout << "3: Display" << endl;
+        cout << "Press command" << endl;
+        cout << "      " << endl;
+        cin >> command;
+        cout << "      " << endl;
+        switch (command) {
+
+        case 1:
+            cout << "Push a number" << endl;
+            cin >> push_number;
+            stack.push(push_number);
+            break;
+        case 2:
+            stack.pop();
+            break;
+        case 3:
+            cout << "Stack contents: ";
+            stack.display();
+         
+            cout << endl;
+            break;
+        case 0:
+            return 0;
+
+        }
+        cout << endl;
+    }
+}
+
